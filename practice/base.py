e92d123376a7a14122de9b84f255fc835fc337e9
Quinn Avila <qavila@drw.com>
2016-06-30 12:25:06 -0500

    oop practice

---
+++ practice/base.py
@@ -0,0 +1,30 @@
+#
+# Probably don't do this
+#
+class BaseClass:
+    num_base_calls = 0
+    def call_me(self):
+        print("Calling method on BaseClass")
+        self.num_base_calls += 1
+
+class LeftSubClass(BaseClass):
+    num_left_calls = 0
+    def call_me(self):
+        BaseClass.call_me(self)
+        print("Calling method on Left Subclass")
+        self.num_left_calls += 1
+
+class RightSubClass(BaseClass):
+    num_right_calls = 0
+    def call_me(self):
+        BaseClass.call_me(self)
+        print("Calling methid on Right Subclass")
+        self.num_right_calls += 1
+
+class Subclass(LeftSubClass, RightSubClass):
+    num_sub_calls = 0
+    def call_me(self):
+        LeftSubClass.call_me(self)
+        RightSubClass.call_me(self)
+        print("Calling method on Subclass")
+        self.num_sub_calls += 1
